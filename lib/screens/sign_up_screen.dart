d6673254dc8da198ccf8c3f9f27a61c4029e6164
Navin Sah <nabinsah703@gmail.com>
2023-02-08 13:58:11 +0545

    sign up with emailAndPassword is successfully

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gettodoapp/screens/login_screen.dart';
 
@@ -11,9 +12,78 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
   TextEditingController confirmPasswordController = TextEditingController();
+
+  var email = '';
+  var password = '';
+  var confirmPassword = '';
+
   final _signUpKey = GlobalKey<FormState>();
+
+  String errorText = "";
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  registration() async {
+    if (password == confirmPassword) {
+      try {
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+        print(userCredential);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              "Registered Successfully. Please Login..",
+            ),
+          ),
+        );
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (builder) => const LoginScreen(),
+            ),
+            (route) => false);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Password Provided is too Weak",
+                style: TextStyle(fontSize: 18.0, color: Colors.black),
+              ),
+            ),
+          );
+        } else if (e.code == 'email-already-in-use') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Account Already exists",
+                style: TextStyle(fontSize: 18.0, color: Colors.black),
+              ),
+            ),
+          );
+        }
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.orangeAccent,
+          content: Text(
+            "Password and Confirm Password doesn't match",
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
+          ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +95,34 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: Form(
         key: _signUpKey,
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextFormField(
                   controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
                       labelText: "Enter Your Email ID")),
               const SizedBox(
-                height: 15,
+                height: 12,
               ),
               TextFormField(
                 controller: passwordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Password';
+                  }
+                  return null;
+                },
                 decoration: const InputDecoration(
                     hintText: "Enter your password",
                     border: OutlineInputBorder(
@@ -48,10 +132,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     )),
               ),
               const SizedBox(
-                height: 15,
+                height: 12,
               ),
               TextFormField(
-                controller: passwordController,
+                controller: confirmPasswordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Password';
+                  }
+                  return null;
+                },
                 decoration: const InputDecoration(
                     hintText: "Confirm password",
                     border: OutlineInputBorder(
@@ -61,10 +151,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     )),
               ),
               const SizedBox(
-                height: 15,
+                height: 12,
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (_signUpKey.currentState!.validate()) {
+                    setState(() {
+                      email = emailController.text;
+                      password = passwordController.text;
+                      confirmPassword = confirmPasswordController.text;
+                    });
+                    registration();
+                  }
+                },
                 child: const Text(
                   "Sign Up",
                 ),
@@ -84,8 +183,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     },
                     child: const Text(
                       "sign in",
-                      style: TextStyle(color: Colors.purple,
-                      fontSize: 18),
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 ],
